c77a98fc086709d7ccb47a531e03fd706c3e7b94
DonatelloLTU <60147196+DonatelloLTU@users.noreply.github.com>
2020-11-16 22:05:10 -0500

    Fixed some db

--- TimesheetLaikasGroup/TimesheetLaikasGroup/Startup.cs
+++ TimesheetLaikasGroup/TimesheetLaikasGroup/Startup.cs
@@ -13,7 +13,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Http;
-//using TimesheetLaikasGroup.Repos;
+using TimesheetLaikasGroup.DAL;
+
 
 namespace TimesheetLaikasGroup
 {
@@ -29,15 +30,17 @@ namespace TimesheetLaikasGroup
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-            services.AddDbContextPool<ApplicationDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Timesheet")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseLazyLoadingProxies().
+                UseSqlServer(
+                  Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            //services.AddDbContextPool<ApplicationDbContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Timesheet")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
